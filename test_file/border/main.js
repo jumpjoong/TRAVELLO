221df391140c659b92fbc59504b5d26a43955eb6
roh96 <74894952+roh96@users.noreply.github.com>
2022-12-29 14:12:46 +0900

    Merge branch 'main' of https://github.com/Team9896/Project-Trip

--- test_file/border/main.js
+++ test_file/border/main.js
@@ -1,10 +1,10 @@
 //마우스 오버시 정지, 오버 아닐 시 재생
-let $slides = document.querySelectorAll('.swiper-slide');
+let $slides = document.querySelectorAll(".swiper-slide");
 for (let i of $slides) {
-  i.addEventListener('mouseover', ()=>{
+  i.addEventListener("mouseover", () => {
     swiper.autoplay.stop();
   });
-  i.addEventListener('mouseout', ()=>{
+  i.addEventListener("mouseout", () => {
     swiper.autoplay.start();
   });
 }
@@ -26,22 +26,21 @@ const swiper = new Swiper(".mySwiper", {
 //오버 시 슬라이드
 let slides, slides1;
 
-swiper.navigation.nextEl.addEventListener('mouseenter',()=>{
-  slides = setInterval(()=>{
+swiper.navigation.nextEl.addEventListener("mouseenter", () => {
+  slides = setInterval(() => {
     swiper.navigation.nextEl.click();
-  }, 1000)
-})
+  }, 1000);
+});
 
-swiper.navigation.prevEl.addEventListener('mouseenter',()=>{
-  slides1 = setInterval(()=>{
+swiper.navigation.prevEl.addEventListener("mouseenter", () => {
+  slides1 = setInterval(() => {
     swiper.navigation.prevEl.click();
-  }, 1000)
-})
+  }, 1000);
+});
 //오버 아닐 시 슬라이드 멈춤
-swiper.navigation.prevEl.addEventListener('mouseout',()=>{
+swiper.navigation.prevEl.addEventListener("mouseout", () => {
   clearInterval(slides1);
-})
-swiper.navigation.nextEl.addEventListener('mouseout',()=>{
+});
+swiper.navigation.nextEl.addEventListener("mouseout", () => {
   clearInterval(slides);
-})
-
+});
