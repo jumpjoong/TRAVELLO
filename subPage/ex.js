f77446d231ee19082794c9e8be84dcfc2672a65e
kimhyunjoong <100519998+khj0196@users.noreply.github.com>
2023-01-02 17:53:26 +0900

    2023/01/02 rework js
    서브페이지 수정

--- subPage/ex.js
+++ subPage/ex.js
@@ -8,45 +8,6 @@ fetch('../resource/data.json')
 //로컬스트로지 값 받오는 변수
 let contryKey = localStorage.getItem('num');
 
-//구글 맵
-// let contryGps = [
-//   {
-//     "contry": "brazil",
-//     "url" : { lat: -14.142426 ,lng: -53.104981 },
-//   },
-//   {
-//     "contry": "canada",
-//     "url" : { lat: 56.00000 ,lng: -96.00000 },
-//   },
-//   {
-//     "contry": "switzerland",
-//     "url" : { lat: 46.584747 ,lng: 8.132716 },
-//   },
-//   {
-//     "contry": "italy",
-//     "url" : { lat: 41.173286 ,lng: 12.342500 },
-//   },
-//   {
-//     "contry": "mongolia",
-//     "url" : { lat: 46.515439 ,lng: 103.50522 },
-//   },
-//   {
-//     "contry": "turkey",
-//     "url" : { lat: 38.572643 ,lng: 35.142667 },
-//   },
-//   {
-//     "contry": " vietnam",
-//     "url" : { lat: 15.541102 ,lng: 105.482409 }
-//   }
-// ]
-// function initMap() {
-//   var map = new google.maps.Map(
-//     document.querySelector('.maps'), {
-//       zoom: 5,
-//       center: contryGps[contryKey].url
-//     }
-//   );
-// }
 //큰 제목
 function init (a) {
   const subEl = document.querySelectorAll('.text > .text-sub')
