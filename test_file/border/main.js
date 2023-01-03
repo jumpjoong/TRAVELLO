26c22d31f11cab4c1635ca61c2e0c064c44a2efe
cj <shwnstlr1@gmail.com>
2023-01-03 18:34:45 +0900

    Merge branch 'main' of https://github.com/Team9896/Project-Trip

--- test_file/border/main.js
+++ test_file/border/main.js
@@ -1,3 +1,20 @@
+let ima=["../img/BRA/brazil_place_01.jpeg",
+"../img/BRA/brazil_place_02.jpg",
+"../img/BRA/brazil_place_03.jpg",
+"../img/BRA/food/brazil01_food01_01.jpg",
+"../img/BRA/food/brazil01_food01_02.jpg",
+"../img/BRA/food/brazil01_food01_03.jpg",
+"../img/BRA/food/brazil01_food01_04.jpg",
+];
+let txt=[["브라질연방 공화국", "FederativeRepublic of Brazil"],
+["튀르키예 공화국" , "Republic of Türkiye"],
+["스위스연방" , "Swiss Confederation"],
+["베트남 사회주의 공화국" , "Socialist Republic of Viet Nam"],
+["캐나다" , "Canada"],
+["아이슬란드 공화국" , "Republic of Iceland"],
+["국", "Country"]
+]
+console.log(txt[3][1]);
 //마우스 오버시 정지, 오버 아닐 시 재생
 let $slides = document.querySelectorAll(".swiper-slide");
 for (let i of $slides) {
@@ -8,6 +25,9 @@ for (let i of $slides) {
     swiper.autoplay.start();
   });
 }
+const visualMain = document.querySelector("main");
+const txtMain= document.querySelector(".center .text h2");
+const txtMain2= document.querySelector(".center .text .text-sub p");
 //스위퍼
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 6,
@@ -22,6 +42,16 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  on:{
+    slideChange:function(e){
+      //visualMain.style=`background-image=${ima[e.realIndex]}`;
+      visualMain.style=`background:url(${ima[e.realIndex]}) 0 0 / cover no-repeat`;
+      txtMain.innerHTML=`${txt[e.realIndex][0]}`;
+      txtMain2.innerHTML=`${txt[e.realIndex][1]}`;
+
+      console.log(e.realIndex)
+    }
+  }
 });
 //오버 시 슬라이드
 let slides, slides1;
