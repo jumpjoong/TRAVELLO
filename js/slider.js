bef8fe6599df3081f713af417b2d19d7c6c76ee4
PronCan <twojini98@gmail.com>
2023-01-07 01:33:20 +0900

    update subpage

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // section3 img slider
 gsap.registerPlugin(ScrollTrigger);
 
@@ -104,3 +105,47 @@ function buildSeamlessLoop(items, spacing) {
 	});
 	return seamlessLoop;
 }
+=======
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".container");
+const mainSlide = document.querySelector(".main-slide");
+const slidesCount = mainSlide.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+// sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+downBtn.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+function changeSlide(direction) {
+  console.log(direction); 
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1;
+    }
+  }
+
+  // const height = container.clientHeight;
+  const width = mainSlide.clientWidth;
+  mainSlide.style.transform = `translateX(-${activeSlideIndex * width/4}px)`;
+}
+
+setInterval(() => {
+// mainSlide.style.transform = translateY(-${activeSlideIndex * height}px);
+// changeSlide('up');
+},3000);
+>>>>>>> Stashed changes
