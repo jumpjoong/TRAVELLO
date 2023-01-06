97a2dc1afb1613ea9919ecca85dec9a118b72ba6
roh96 <74894952+roh96@users.noreply.github.com>
2023-01-06 14:33:47 +0900

    change main

--- test_file/border/main.js
+++ test_file/border/main.js
@@ -1,19 +1,21 @@
-let ima=["../img/br/brazil_place_01.jpeg",
-"../img/br/tur_place_01.jpg",
-"../img/br/che_place_01.png",
-"../img/br/vietnam_place_01.jpg",
-"../img/br/can_place_02.png",
-"../img/br/isl_place_01.jpg",
-"../img/br/ita_place_03.jpg",
+let ima = [
+  "./img/br/brazil_place_01.jpeg",
+  "./img/br/tur_place_01.jpg",
+  "./img/br/che_place_01.png",
+  "./img/br/vietnam_place_01.jpg",
+  "./img/br/can_place_02.png",
+  "./img/br/isl_place_01.jpg",
+  "./img/br/ita_place_03.jpg",
 ];
-let txt=[["브라질연방 공화국", "FederativeRepublic of Brazil"],
-["튀르키예 공화국" , "Republic of Türkiye"],
-["스위스연방" , "Swiss Confederation"],
-["베트남 사회주의 공화국" , "Socialist Republic of Viet Nam"],
-["캐나다" , "Canada"],
-["아이슬란드 공화국" , "Republic of Iceland"],
-["이탈리아 공화국", "The Italian Republic"]
-]
+let txt = [
+  ["브라질연방 공화국", "FederativeRepublic of Brazil"],
+  ["튀르키예 공화국", "Republic of Türkiye"],
+  ["스위스연방", "Swiss Confederation"],
+  ["베트남 사회주의 공화국", "Socialist Republic of Viet Nam"],
+  ["캐나다", "Canada"],
+  ["아이슬란드 공화국", "Republic of Iceland"],
+  ["이탈리아 공화국", "The Italian Republic"],
+];
 console.log(txt[3][1]);
 //마우스 오버시 정지, 오버 아닐 시 재생
 let $slides = document.querySelectorAll(".swiper-slide");
@@ -25,11 +27,11 @@ for (let i of $slides) {
     swiper.autoplay.start();
   });
 }
-const visualMain = document.querySelector(".swi");
-const txtMain= document.querySelector(".center .text h2");
-const txtMain2= document.querySelector(".center .text .text-sub p");
-const txtM= document.querySelector(".center .text");
-let iddx=0;
+const visualMain = document.querySelector("main");
+const txtMain = document.querySelector(".center .text h2");
+const txtMain2 = document.querySelector(".center .text .text-sub p");
+const txtM = document.querySelector(".center .text");
+let iddx = 0;
 //스위퍼
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 6,
@@ -44,38 +46,40 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  on:{
-    slideChange:function(e){
+  on: {
+    slideChange: function (e) {
       //visualMain.style=`background-image=${ima[e.realIndex]}`;
-      visualMain.style=`background:url(${ima[e.realIndex]}) center center  / cover no-repeat`;
-      txtM.classList.remove('active');
-      
+      visualMain.style = `background:url(${
+        ima[e.realIndex]
+      }) center center  / cover no-repeat`;
+      txtM.classList.remove("active");
+
       setTimeout(() => {
-        txtMain.innerHTML=`${txt[e.realIndex][0]}`;
-        txtMain2.innerHTML=`${txt[e.realIndex][1]}`;
-        txtM.classList.add('active');     
+        txtMain.innerHTML = `${txt[e.realIndex][0]}`;
+        txtMain2.innerHTML = `${txt[e.realIndex][1]}`;
+        txtM.classList.add("active");
       }, 1000);
       //console.log(e.realIndex)
-    }
+    },
   },
   breakpoints: {
-    1920:{
+    1920: {
       slidesPerView: 6,
       spaceBetween: 56,
     },
-    1280:{
-      slidesPerView:5,
-      spaceBetween:90
+    1280: {
+      slidesPerView: 5,
+      spaceBetween: 90,
     },
-    1024:{
-      slidesPerView:4,
-      spaceBetween:40
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-    768:{
-      slidesPerView:3,
-      spaceBetween:60
-    }
-  }
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+  },
 });
 //오버 시 슬라이드
 let slides, slides1;
